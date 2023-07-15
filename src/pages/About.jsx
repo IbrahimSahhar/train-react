d90ec04f768963abbf3d1b3dda0825c1c96d9a99
Ibrahim <113946679+IbrahimSahhar@users.noreply.github.com>
2023-07-15 10:58:26 +0300

    add the typography commponent with some styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
 import Container from "../components/Container";
+import { H1, H2, H3, H4, H5, H6, P, Span } from "../components/Typography";
 
 export default class About extends Component {
   state = {
@@ -21,6 +22,16 @@ export default class About extends Component {
             add
           </button>
           <span className="count">{this.state.count}</span>
+
+          <div style={{ margin: "20px 0" }}>Typography</div>
+          <H1>Title with h1</H1>
+          <H2>Title with h2</H2>
+          <H3>Title with h3</H3>
+          <H4>Title with h4</H4>
+          <H5>Title with h5</H5>
+          <H6>Title with h6</H6>
+          <P>Title with paragraph</P>
+          <Span>Title with span</Span>
         </Container>
       </div>
     );
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { H1 } from "../components/Typography";
 
 export default class Tasks extends Component {
   state = {
@@ -10,12 +11,36 @@ export default class Tasks extends Component {
       theme: prev.theme === "dark" ? "light" : "dark",
     }));
   };
+
+  mainStyle = {
+    padding: "10px 20px",
+    border: "none",
+  };
+
+  darkStyle = {
+    ...this.mainStyle,
+    backgroundColor: "black",
+    color: "white",
+  };
+  lightStyle = {
+    ...this.mainStyle,
+    backgroundColor: "white",
+    color: "black",
+  };
   render() {
+    console.log("Typography");
     return (
       <div>
         <div className="task-1">
-          <h1> Task 1</h1>
-          <button onClick={this.changeTheme}>{this.state.theme}</button>
+          <H1> Task 1</H1>
+          <button
+            style={
+              this.state.theme === "dark" ? this.darkStyle : this.lightStyle
+            }
+            onClick={this.changeTheme}
+          >
+            {this.state.theme === "dark" ? "light" : "dark"}
+          </button>
         </div>
       </div>
     );
